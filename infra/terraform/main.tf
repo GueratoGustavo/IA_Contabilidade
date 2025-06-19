9eb5dd0f6a75f4b49d2de79ea356b7902fb33c8e
GueratoGustavo <githubgustavo48@gmail.com>
2025-06-19 00:14:30 -0300

    comeco da criação da minha IA

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,10 +1,36 @@
+terraform {
+  required_providers {
+    k3s = {
+      source  = "rancher/k3s"
+      version = "1.0.1"
+    }
+  }
+
+  required_version = ">= 1.6.0"
+}
+
+provider "k3s" {
+  kubeconfig_output = "./kubeconfig.yaml"
+}
+
+resource "k3s_cluster" "agente_financeiro" {
+  cluster_name = "agente-financeiro"
+  servers = 1
+
+  agents = 1
+
+  cluster_config {
+    disable = ["traefik"]
+  }
+}
+
 provider "kubernetes" {
-  config_path = pathexpand("~/.kube/config")
+  config_path = "./kubeconfig.yaml"
 }
 
 provider "helm" {
   kubernetes {
-    config_path = pathexpand("~/.kube/config")
+    config_path = "./kubeconfig.yaml"
   }
 }
 
@@ -18,7 +44,7 @@ resource "helm_release" "nginx" {
   name       = "nginx"
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "nginx"
-  version    = "13.2.9"  # substitua pela versão válida encontrada
+  version    = "13.2.9"
 
   namespace = kubernetes_namespace.agente_financeiro.metadata[0].name
 
