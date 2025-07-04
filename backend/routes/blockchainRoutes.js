62d13c0a9ba75dd2a7d58affd102e36a97dc0c88
GueratoGustavo <githubgustavo48@gmail.com>
2025-07-03 22:07:03 -0300

    criacao do blockchain via WSL

--- backend/routes/blockchainRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authMiddleware = require("../middlewares/authMiddleware");
-const { verifyBlockchain } = require("../controllers/blockchainController");
-
-router.get("/verify", authMiddleware, verifyBlockchain);
-
-module.exports = router;
